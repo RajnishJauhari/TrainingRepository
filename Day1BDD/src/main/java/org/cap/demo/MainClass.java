0212e3dd138625694762626c5ce82ca32d9e8d99
rjauhari <rjauhari@DIN73002983.corp.capgemini.com>
2018-11-23 15:10:02 +0530

    Hey My First Commit!

---
+++ Day1BDD/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,17 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		sayHello();
+		
+	}
+	
+	public static void sayHello() {
+		
+		System.out.println("Hello!");
+		
+	}
+
+}
